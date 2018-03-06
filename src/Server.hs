750466db3e492cb1430811c5259fecd1dfc6291a
Paweł Nosal <p.nosal1986@gmail.com>
2018-03-06 21:20:15 +0100

    add swagger

--- src/Server.hs
+++ src/Server.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 module Server ( application ) where
 
 import Servant
@@ -8,17 +9,24 @@ import Users.Instances
 import Errors.Instances
 import Users.Types (User)
 import Data.Aeson
+import Servant.Swagger.UI
+import Servant.Swagger
+import Data.Swagger
 
 instance ToJSON User
 instance FromJSON User
+instance ToSchema User
 
-type Api = ApplicationAPI
+type Api = SwaggerSchemaUI "swagger-ui" "swagger.json" :<|> ApplicationAPI
 
 apiImplementation :: Server Api
-apiImplementation = userApi
+apiImplementation = swaggerSchemaUIServer swaggerDoc :<|> userApi
 
 applicationAPI :: Proxy Api
 applicationAPI = Proxy
 
 application :: Application
 application = serve applicationAPI apiImplementation
+
+swaggerDoc :: Swagger
+swaggerDoc = toSwagger (Proxy :: Proxy ApplicationAPI)
