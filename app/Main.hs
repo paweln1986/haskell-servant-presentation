34c313b16cba287bed17e6c2324bffa9a673a816
Paweł Nosal <p.nosal1986@gmail.com>
2018-03-06 21:57:16 +0100

    add EKG

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Server
+import Server (generateJsClients, application)
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import Network.Wai.Handler.Warp    (run)
+import Network.Wai.Handler.Warp (run)
+import Network.Wai (Middleware)
+import Network.Wai.Metrics (registerWaiMetrics, metrics)
+import System.Metrics (registerGcMetrics, newStore)
+import System.Remote.Monitoring (serverMetricStore, forkServerWith)
+
+ekg :: Int -> IO Middleware
+ekg port = do
+    s <- newStore
+    registerGcMetrics s
+    wai <- registerWaiMetrics s
+    _ <- forkServerWith s "localhost" port
+    pure (metrics wai)
 
 main :: IO ()
 main = do
   putStrLn "Starting server on port 8081"
   generateJsClients
-  run 8081 $ logStdoutDev application
+  waiMetrics <- ekg 8000
+  run 8081 $ waiMetrics $ logStdoutDev application
