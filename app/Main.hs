233cdad0d86685a1c0ce85f4cdef2a663e6cbea4
Paweł Nosal <p.nosal1986@gmail.com>
2018-03-06 21:39:41 +0100

    add generate js clients

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,11 @@
 module Main where
 
 import Server
-
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Network.Wai.Handler.Warp    (run)
 
 main :: IO ()
 main = do
   putStrLn "Starting server on port 8081"
+  generateJsClients
   run 8081 $ logStdoutDev application
--- src/Server.hs
+++ src/Server.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
-module Server ( application ) where
+module Server ( application, generateJsClients) where
 
 import Servant
 import Servant.Server
@@ -12,6 +12,8 @@ import Data.Aeson
 import Servant.Swagger.UI
 import Servant.Swagger
 import Data.Swagger
+import Servant.JS
+import System.FilePath
 
 instance ToJSON User
 instance FromJSON User
@@ -25,13 +27,20 @@ www = "examples/www"
 apiImplementation :: Server Api
 apiImplementation = swaggerSchemaUIServer swaggerDoc :<|> userApi :<|> serveDirectoryFileServer www
 
+userApiProxy :: Proxy ApplicationAPI
+userApiProxy = Proxy
+
 applicationAPI :: Proxy Api
 applicationAPI = Proxy
-
-
 
 application :: Application
 application = serve applicationAPI apiImplementation
 
 swaggerDoc :: Swagger
 swaggerDoc = toSwagger (Proxy :: Proxy ApplicationAPI)
+
+generateJsClients = do
+  writeJSForAPI userApiProxy jquery (www </> "api.js")
+  writeJSForAPI userApiProxy vanillaJS (www </> "vanillajs-api.js")
+  writeJSForAPI userApiProxy (angular defAngularOptions) (www </> "angular-api.js")
+  writeJSForAPI userApiProxy (axios defAxiosOptions) (www </> "axios-api.js")
